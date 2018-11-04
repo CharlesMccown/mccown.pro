6b9e37ef69e1024e591ba5295d4ed2bb316f660c
Charles A. McCown <charlesmccown@gmail.com>
2018-11-04 14:49:14 -0600

    First pass on home page

--- copy-dev.sh
+++ copy-dev.sh
@@ -1,8 +1,8 @@
 sudo service nginx stop
 
 sudo cp -r ./src/* /var/www/mccown.pro
-sudo cp ./config/nginx-conf /etc/nginx/sites-available/nginx-conf
+sudo cp ./config/nginx.conf /etc/nginx/sites-available/nginx.conf
 sudo find /etc/nginx/sites-enabled/ -type l -print0 | sudo xargs -0 rm
-sudo ln -s /etc/nginx/sites-available/nginx-conf /etc/nginx/sites-enabled/nginx-conf
+sudo ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/nginx.conf
 
 sudo service nginx start
